c14d8e557231c5cd47d07d492b205e7cdb1a6553
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-19 13:37:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,8 +33,8 @@ resource "aws_ecs_cluster_capacity_providers" "provider" {
     "FARGATE_SPOT"
   ]
 
-  default_capacity_provider_strategy = {
-    capacity_provider = "FARGATE"
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
     weight            = 1
     
   }
